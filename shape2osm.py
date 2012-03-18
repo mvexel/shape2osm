f6dec64b9911bc1898e25437507dd1bcb1e3b585
Martijn van Exel <mvexel@gmail.com>
2012-03-17 22:07:53 -0600

    initial commit

---
+++ shape2osm.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+
+# shape2osm.py
+# ============
+#
+# this is a single purpose program to convert POINT shapefiles to .OSM
+# files. Do not feed it anything else.
+#
+# Setup
+# =====
+# Prerequisites:
+# * [pyshp](http://code.google.com/p/pyshp/) (included)
+# * [progress_bar](http://coreygoldberg.blogspot.com/2010/01/python-command-line-progress-bar-with.html) (included)
+# * [elementtree](http://effbot.org/zone/xml-writer.htm)
+# * [argparse](http://code.google.com/p/argparse/) (included in python 2.7 and later)
+# Install these, chmod 0755 shape2osm.py and you should be good to go. 
+# 
+# Usage
+# ===== 
+# shape2osm.py [-h] INFILE OUTFILE
+#
+# License
+# =======
+# Copyright (c) 2012 Martijn van Exel
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the 
+# "Software"), to deal in the Software without restriction, including 
+# without limitation the rights to use, copy, modify, merge, publish, 
+# distribute, sublicense, and/or sell copies of the Software, and to 
+# permit persons to whom the Software is furnished to do so, subject 
+# to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be 
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+
+__version__ = 0.1
+
+import sys
+import shapefile
+import argparse
+from progress_bar import ProgressBar
+from elementtree.SimpleXMLWriter import XMLWriter
+from datetime import datetime
+
+API_VERSION = "0.6"
+
+parser = argparse.ArgumentParser(description='Convert a ESRI Shapefile (POINT only) to .OSM')
+parser.add_argument('INFILE', help='The path to the input ESRI shapefile, will append .shp if omitted')
+parser.add_argument('OUTFILE', type=argparse.FileType('w'), help='The path to the output OSM XML file')
+args = parser.parse_args()
+
+osm_id = 0
+dt = datetime.now()
+
+sf = shapefile.Reader(args.INFILE)
+f = sf.fields
+l = len(sf.shapes())
+p = ProgressBar(l)
+
+w = XMLWriter(args.OUTFILE)
+w.start("osm", {"generator": "shape2osm " + str(__version__), "version": API_VERSION})
+for shape in sf.shapeRecords():
+    osm_id -= 1
+    (x,y) = shape.shape.points[0]
+    w.start("node", {"id": str(osm_id), "timestamp": dt.isoformat(), "version": "1", "visible": "true", "lon": str(x), "lat": str(y)})
+    for i in range(1,len(f)):
+        w.element("tag", "", {"k": f[i][0], "v": shape.record[i-1]})
+    w.end()
+    p.update_time(l - (l + osm_id))
+    print "{0}\r".format(p),
+w.end()
+print "\nfinished."
